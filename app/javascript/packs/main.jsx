ae5737b6deaaa1636506006543ad91a54b2025c1
Rayner Lim <rlrh1996@gmail.com>
2018-12-15 11:18:42 +0800

    Bootstrap front end

--- app/javascript/packs/main.jsx
+++ app/javascript/packs/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.body.style = 'background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);';
     ReactDOM.render(
         <Main/>,
         document.body.appendChild(document.createElement('div')),
@@ -27,19 +28,35 @@ class Main extends React.Component {
 
     render() {
         return (
-            <main className="nusmods-main">
-                <div>
-                    <h3>Todolist On React Rails</h3>
-                    <form onSubmit={this.handleSubmit}>
-                        <input
-                            className="input"
-                            onChange={this.handleChange}
-                            value={this.state.text}
-                            placeholder="What do you need to do? (Press Enter for new to-do)"
-                            autoFocus="true"
-                        />
-                    </form>
-                    <TodoList items={this.state.items} deleteHandler={this.handleDelete} completeHandler={this.handleComplete} editHandler={this.handleEdit} />
+            <main>
+                <div className="container">
+                    <div className="row mt-5 d-flex justify-content-between">
+                        <h1 className="display-4 text-white">Todolist</h1>
+                        <span className="text-white">Built with Ruby on Rails, React and Bootstrap</span>
+                    </div>
+                    <div className="row mb-5">
+                        <div className="col-2">
+                        </div>
+                        <div className="col">
+                            <div className="row mt-3">
+                                <form className="container" onSubmit={this.handleSubmit}>
+                                    <input
+                                        className="shadow form-control form-control-lg"
+                                        onChange={this.handleChange}
+                                        value={this.state.text}
+                                        placeholder="What do you need to do?"
+                                        autoFocus
+                                    />
+                                </form>
+                                
+                            </div>
+                            <div className="row mt-3">
+                                <div className="container">
+                                    <TodoList items={this.state.items} deleteHandler={this.handleDelete} completeHandler={this.handleComplete} editHandler={this.handleEdit} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         );
@@ -153,9 +170,9 @@ class Main extends React.Component {
 class TodoList extends React.Component {
     render() {
         return (
-            <ul>
+            <ul className="shadow list-group">
                 {this.props.items.map(item => <Todo key={item.id} id={item.id} text={item.description} completed={item.completed} deleteHandler={this.props.deleteHandler} completeHandler={this.props.completeHandler} editHandler={this.props.editHandler} />)}
-            </ul>
+            </ul>       
         );
     }
 }
@@ -181,33 +198,47 @@ class Todo extends React.Component {
 
         if (this.state.editing) {
             return (
-                <li key={this.props.id}>
-                    <input onChange={this.handleChange} value={this.state.text} />
-                    <button
-                        onClick={(e) => {
-                            let editedTodo = {id: this.props.id, description: this.state.text};
-                            this.props.editHandler(editedTodo);
-                            this.handleEditing(e);
-                        }}
-                    >
-                        Done
-                    </button>
+                <li className="list-group-item" key={this.props.id} >
+                    <div class="input-group">
+                        <input className="form-control form-control-sm" onChange={this.handleChange} value={this.state.text} />
+                        <div class="input-group-append">
+                        <button
+                            className="btn btn-primary btn-sm"
+                            onClick={(e) => {
+                                let editedTodo = {id: this.props.id, description: this.state.text};
+                                this.props.editHandler(editedTodo);
+                                this.handleEditing(e);
+                            }}
+                        >
+                            Done
+                        </button>
+                        </div>
+                    </div>
                 </li>
             );
         } else {
             return (
-                <li key={this.props.id}>
-                    <input
-                        type="checkbox"
-                        checked={this.props.completed}
-                        onChange={() => {
-                            let editedTodo = {id: this.props.id, completed: !this.props.completed};
-                            this.props.completeHandler(editedTodo);
-                        }}
-                    />
-                    {this.props.completed ? <span className="strike">{this.state.text}</span> : <span>{this.state.text}</span>}
-                    <button className="right" onClick={() => this.props.deleteHandler(this.props.id)}>Delete</button>
-                    <button className="right" onClick={this.handleEditing}>Edit</button>
+                <li className={this.props.completed ? "list-group-item disabled d-flex justify-content-between align-items-center" : "list-group-item d-flex justify-content-between align-items-center"} key={this.props.id}>
+                    <div className="flex-grow-1">
+                        <div class="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                checked={this.props.completed}
+                                onChange={() => {
+                                    let editedTodo = {id: this.props.id, completed: !this.props.completed};
+                                    this.props.completeHandler(editedTodo);
+                                }}
+                            />
+                            <label class="form-check-label">
+                                {this.props.completed ? <del>{this.state.text}</del> : <span>{this.state.text}</span>}
+                            </label>
+                        </div>
+                    </div>
+                    <div className="btn-group btn-group-sm" role="group">
+                        <button className="btn btn-light" onClick={this.handleEditing}>Edit</button>
+                        <button className="btn btn-light" onClick={() => this.props.deleteHandler(this.props.id)}>Delete</button>
+                    </div>
                 </li>
             );
         }
